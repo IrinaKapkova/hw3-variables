a01f95e3f7cd6280b854502d3af742134b3f8b10
Ирина Капкова <kapiral@rambler.ru>
2022-09-23 10:23:14 +0400

    3 задание

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,21 @@ public class Main {
     public static void main(String[] args) {
 
 
-        // Задача 6
-        System.out.println("Задача 6");
+        // Задача №6
+        System.out.println("Задача №6");
         var weight1boxer = 78.2;
         var weight2boxer = 82.7;
         System.out.println("Общий вес двух бойцов:  " + (weight1boxer + weight2boxer));
         System.out.println("Разница между весами бойцов: " + (weight2boxer - weight1boxer));
 
 
-        // Задача 7
-        System.out.println("Задача 7 ");
+        // Задача №7
+        System.out.println("Задача №7");
         System.out.println("Разница между весами бойцов (Первый способ): " + (weight2boxer - weight1boxer));
         System.out.println("Разница между весами бойцов (Второй способ): " + (weight2boxer % weight1boxer));
 
-        // Задача 8
-        System.out.println("Задача 8");
+        // Задача №8
+        System.out.println("Задача №8");
         var totalWorkingTime = 640;
         var lengthWorkingDay = 8;
         var amountWorkers = totalWorkingTime / lengthWorkingDay;
